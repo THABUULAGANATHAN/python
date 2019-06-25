1d5c0a675f6a801e01db58d587766fc28b99eb71
THABUULAGANATHAN <51949567+THABUULAGANATHAN@users.noreply.github.com>
2019-06-25 11:39:10 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+import sys
+vow='aeiouAEIOU'
+t='BCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz'
+c=input()
+if c in vow:
+  print("Vowe1")
+elif c in t:
+  print("Consonant")
+else:
+  print("invalid")
+  
