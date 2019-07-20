3dbebbdeab0666dde96eedb97ef122f9862292d3
THABUULAGANATHAN <51949567+THABUULAGANATHAN@users.noreply.github.com>
2019-07-20 09:21:21 +0530

    Create pro18.py

---
+++ pro18.py
@@ -0,0 +1,17 @@
+aprt,brt=map(int,input().split())
+l1t=[]
+for _ in range(aprt):
+    l1t.append(input())
+for ic in range(len(l1t)):
+    if('0' in l1t[ic]):
+        l1t[ic]=l1[ic].replace('0','')
+    l1t[ic]=l1t[ic].replace(' ','')
+lengths=[]
+for ic in l1t:
+    if(len(ic)>0):
+        lengths.append(len(ic))
+brt=min(lengths)
+rt1='1 '*brt
+rt1=rt1.strip()
+for ic in range(brt):
+    print(rt1)
