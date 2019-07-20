e332ec5ae479c478db313578d4a6766711f1379b
THABUULAGANATHAN <51949567+THABUULAGANATHAN@users.noreply.github.com>
2019-07-20 10:27:35 +0530

    Update pro24.py

--- pro24.py
+++ pro24.py
@@ -1,9 +1,9 @@
-nt=int(input())
-bt=pow(2,nt)
-at=[]
-for i in range(bt):  
-    ct=bin(i).replace("0bt","")
-    at.append(ct.zfill(nt))
-    at.sort(key=(lambda xt:xt.count('1')))
-for i in at:
+at1=int(input())
+bt1=pow(2,at1)
+zt1=[]
+for i in range(bt1):  
+    mt1=bin(i).replace("0b","")
+    zt1.append(mt1.zfill(at1))
+    zt1.sort(key=(lambda x:x.count('1')))
+for i in zt1:
     print(i)
