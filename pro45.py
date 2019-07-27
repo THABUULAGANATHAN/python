c493df15dcbff5a411b71faff5ac38fa61da8924
THABUULAGANATHAN <51949567+THABUULAGANATHAN@users.noreply.github.com>
2019-07-27 09:38:12 +0530

    Create pro45.py

---
+++ pro45.py
@@ -0,0 +1,18 @@
+import sys, string, math
+
+samt = input()
+if samt == samt[::-1] :
+    print('yes')
+    sys.exit()
+kimt = 0
+for cust in samt[::-1] :
+    if cust == '0' :
+        kimt += 1
+    else :
+        break
+sopt = '0'*kimt + samt
+
+if sopt == sopt[::-1] :
+    print('no')
+else :
+    print('yes')
